90593877f1ce5b3b901567e7eb2ecf49ab8221c9
TheSolipsist <57457393+TheSolipsist@users.noreply.github.com>
2024-08-19 19:52:28 +0200

    Abstract player implementation

--- game/player.py
+++ game/player.py
@@ -2,14 +2,51 @@
 Module containing abstract Player class and all specific player classes
 """
 
+from __future__ import annotations
+
 from constants import SideType
+from exceptions import SelfCaptureError
+
 from abc import ABC
 
 
 class Player(ABC):
+    """Abstract base class representing a player.
+    
+    Attributes:
+        side (SideType): The player's side (White or Black in standard mode).
+        captured_pieces (list[Piece]): Pieces that the player has captured.
+    
+    Methods:
+        capture (piece: Piece): Captures another player's piece
     """
-    Abstract player class
-    """
+    
     def __init__(self, side: SideType) -> None:
-        self.side = side
-        self.captured_pieces = []
+        self._side: SideType = side
+        self._captured_pieces: list["Piece"] = []
+        
+    @property
+    def side(self) -> SideType:
+        """The player's side (White or Black in standard mode)"""
+        return self._side
+    
+    @property
+    def captured_pieces(self) -> list("Piece"):
+        """All the pieces that the player has captured"""
+        return self._captured_pieces
+
+    def capture(self, piece: "Piece") -> None:
+        """Captures a piece, adding it to the list of captured pieces.
+        
+        Args:
+            piece (Piece): The piece to be captured.
+        
+        Raises:
+            SelfCaptureError: The player attempted to capture his own piece
+        """
+        if piece.player is self:
+            raise SelfCaptureError(
+                f"Player {self} attempted to capture his "
+                f"own piece {piece}"
+            )
+        self.captured_pieces.append(piece)
